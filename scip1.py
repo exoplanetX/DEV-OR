ab0d2181cfe38cb539ff0321d57d474a07489201
explanetX <31415361+exoplanetX@users.noreply.github.com>
2023-06-06 15:40:27 +0800

    0606-2

---
+++ scip1.py
@@ -0,0 +1,12 @@
+import pyscipopt
+from pyscipopt import Model
+
+model = Model("Example")
+x = model.addVar("x")
+y = model.addVar("y", vtype="INTEGER")
+model.setObjective(x + y)
+model.addCons(2*x - y*y >= 0)
+model.optimize()
+sol = model.getBestSol()
+print("x: {}".format(sol[x]))
+print("y: {}".format(sol[y]))
